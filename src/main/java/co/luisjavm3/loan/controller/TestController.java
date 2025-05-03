c753ecc4c782d4aa0c44aa22186b3cf925e0c017
Luis J. Montes <luisjavm3@gmail.com>
2025-05-03 15:49:40 -0500

    v4

--- src/main/java/co/luisjavm3/loan/controller/TestController.java
+++ src/main/java/co/luisjavm3/loan/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "Loan Service - OK - v3";
+        return "Loan Service - OK - v4";
     }
 }
