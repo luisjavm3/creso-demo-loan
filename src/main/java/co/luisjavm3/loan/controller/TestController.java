a074143dda954c572fb47512767fcac16247efcd
Luis Javier <luisjavm3@gmail.com>
2025-05-12 17:27:56 -0500

    add instance ID test

--- src/main/java/co/luisjavm3/loan/controller/TestController.java
+++ src/main/java/co/luisjavm3/loan/controller/TestController.java
@@ -2,6 +2,7 @@ package co.luisjavm3.loan.controller;
 
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Value("${instance.id}")
+    private String instanceId;
+
     @GetMapping
     public String test() {
         return "Loan Service : OK";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Loan Service : v1";
+    }
+
+    @GetMapping("/instance-id")
+    public String getInstanceId() {
+        String shortId = instanceId.length() > 4 ? instanceId.substring(0, 4) : instanceId;
+        return "Instance ID: " + shortId;
     }
 
     @GetMapping("/loop")
